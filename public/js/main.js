bdb0fc3f16e9c3df7dfdf670a95f965fee8f7f07
Jordao Serafim <jordaoesa@gmail.com>
2016-12-05 01:38:01 -0300

    5th class.. ngRoute, partial view, html5Mode, base (/)

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,18 @@
  */
 
 
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate']);
+var app = angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute']);
+app.config(function ($routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.otherwise({ redirectTo : '/fotos'});
+
+    $routeProvider.when('/fotos', {
+        templateUrl: 'partials/principal.html',
+        controller: 'FotosController'
+    });
+    $routeProvider.when('/fotos/new', {
+        templateUrl: 'partials/foto.html'
+    });
+});
